772ffdb98f93010d61672b2fe4e32d5a10c156b3
Patrick Finkbeiner <finkbeiner.patrick@gmail.com>
2012-06-18 12:50:39 +0200

    Revert "[TASK] Use it as a jQuery plugin"
    This reverts commit cb0c0ebd871c1b24f59996d0d6d125f19f172203.

--- JavaScript/centralizeIt.js
+++ JavaScript/centralizeIt.js
@@ -1,21 +1,22 @@
-(function($){
-    $.fn.extend({ 
-        centralizeIt: function() {
-			var selector = this;
-			this.calc(selector);
-			$(window).resize(function(){
-				selector.calc(selector);
-			});
-        },
-		calc: function(selector) {
-			console.log(selector);
-			var totalHeight = selector.outerHeight();
-			var windowHeight = $(document).height();
-			var margin = Math.floor((windowHeight - totalHeight) / 2 );
-			this.setPosition(selector, margin);
-		},
-		setPosition: function(selector, margin) {
-			selector.css('margin-top', margin)
-		}
-    });
-})(jQuery);
+var centralize = {
+	mainContainer: 0,
+	windowHeight: undefined,
+	totalHeight: 0,
+	margin: 0,
+	it: function() {
+		var me = this;
+		me.calculate();	
+		$(window).resize(function(){
+			me.calculate();	
+		});
+	},
+	calculate: function() {
+		var me = this;
+		me.mainContainer = $('.wrapper');
+		me.totalHeight = me.mainContainer.outerHeight();
+		me.windowHeight = $(document).height();
+		me.margin = Math.floor((me.windowHeight - me.totalHeight) / 2 );
+		me.mainContainer.css('margin-top', me.margin)
+	}
+
+}
